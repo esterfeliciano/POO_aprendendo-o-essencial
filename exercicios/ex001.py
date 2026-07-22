6a9aca822728a01db91be53344f68af8a7e9f648
esterfeliciano <ester.felicianomelo@gamil.com>
2026-07-22 14:31:52 -0300

    POO-MENSAGEM: Commit para trocar de computador

--- exercicios/ex001.py
+++ exercicios/ex001.py
@@ -9,15 +9,25 @@ class Gafanhoto:
     def aniversario(self):
             self.idade = self.idade + 1
 
-    def mensagem(self):
+    def __str__(self):
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+    def __getstate__(self):
+         return f"Estado:" nome = {self.nome} ; idade = {self.idade}
 # Declaração de objetos
 g1 = Gafanhoto(n="Maria", i =17)
 g1.aniversario()
-print(g1.mensagem())
+print(g1())
 
 g2 = Gafanhoto(n="José", i =45)
-print(g2.mensagem())
+print(g2())
 
 g3 = Gafanhoto(n="Antônio", i =67)
-print(g3.mensagem)
+print(g3)
+
+# print (g1)
+print(g1.dict__) #Attribute
+print(g1.__getstate__())
+
+# print(g1.__doc__) # Dunder Attribute
+
+g2 = Gafanhoto nome:"Mauro
