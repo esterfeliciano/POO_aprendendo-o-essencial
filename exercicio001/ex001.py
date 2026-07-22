61d9611d91ad5b0c30e6396ae3a8b98a0da69a09
esterfeliciano <ester.felicianomelo@gamil.com>
2026-07-22 14:13:37 -0300

    POO-MENSAGEM: Como melhorando as classes

--- exercicio001/ex001.py
+++ exercicio001/ex001.py
@@ -1,20 +1,23 @@
 # Declaração de classe 
 class Gafanhoto:
-    def __init__(self): # Método Constutor
+    def __init__(self, n = "", i = 0): # Método Constutor
          # Atributos de Instãncia
-         self.nome = ""
-         self.idade = 0
+         self.nome = n
+         self.idade = i
 
-         # Métodos de Instâcia
-
-    
+    # Métodos de Instâcia
     def aniversario(self):
             self.idade = self.idade + 1
 
     def mensagem(self):
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
 # Declaração de objetos
-g1 = Gafanhoto()
-g1.nome = "Maria"
-g1.idade = 1
+g1 = Gafanhoto(n="Maria", i =17)
+g1.aniversario()
 print(g1.mensagem())
+
+g2 = Gafanhoto(n="José", i =45)
+print(g2.mensagem())
+
+g3 = Gafanhoto(n="Antônio", i =67)
+print(g3.mensagem)
