f97fbce926ca461f0f00dfed3797130bdef9cd3e
esterfeliciano <ester.felicianomelo@gamil.com>
2026-07-22 13:39:49 -0300

    POO-MENSAGEM: Ajustando a identação do código

--- exercicio001/ex001.py
+++ exercicio001/ex001.py
@@ -7,14 +7,14 @@ class Gafanhoto:
 
          # Métodos de Instâcia
 
-         def aniversario(self):
-              self.idade = self.idade + 1
+    
+    def aniversario(self):
+            self.idade = self.idade + 1
 
-         def mensagem(self):
-              return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
 # Declaração de objetos
 g1 = Gafanhoto()
 g1.nome = "Maria"
-g1.idade = 17
-g1.aniversario()
+g1.idade = 1
 print(g1.mensagem())
